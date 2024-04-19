72386819a10306ea841a246c0f226ecf887a4e11
sanjith211 <sanjith211@gmail.com>
2024-04-19 18:51:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [product, setProducts] = useState([]);
+  const [id, setId] = useState(0);
+
+  useEffect(() => {
+    try {
+      axios
+        .get("https://dummyjson.com/products")
+        .then((res) => setProducts(res.data.products));
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  }, []);
+
+  console.log("length", product.length);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setId((prevIndex) => (prevIndex + 1) % product.length);
+    }, 2000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [product]);
+
+  console.log("index", id);
+
+  var currentProduct = product[id];
+
+  console.log("current", currentProduct);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {product.length > 0 ? (
+        <div>
+          <div>
+            <strong>Name:</strong> {currentProduct.title}
+          </div>
+          <div>
+            <img src={currentProduct.images} alt={"Text"} />
+          </div>
+          <div>
+            <strong>Category:</strong> {currentProduct.category}
+          </div>
+          <div>
+            <strong>Price:</strong> {currentProduct.price}
+          </div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
